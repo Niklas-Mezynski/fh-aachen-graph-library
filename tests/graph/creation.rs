47d0935ff4df5897d3a80b95802fc0b983b57aa6
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2025-03-31 17:16:05 +0200

    Add tests for graph creation (all graph sizes)

--- tests/graph/creation.rs
+++ tests/graph/creation.rs
@@ -2,12 +2,20 @@ use graph_library::{Graph, GraphInterface};
 use rstest::rstest;
 use std::time::Instant;
 
-#[test]
-fn create_from_file() {
-    let graph =
-        Graph::from_hoever_file("resources/test_graphs/undirected/Graph1.txt", false).unwrap();
+#[rstest]
+#[case("resources/test_graphs/undirected/Graph1.txt", 15)]
+#[case("resources/test_graphs/undirected/Graph2.txt", 1000)]
+#[case("resources/test_graphs/undirected/Graph3.txt", 1000)]
+#[case("resources/test_graphs/undirected/Graph_gross.txt", 100000)]
+#[case("resources/test_graphs/undirected/Graph_ganzgross.txt", 500000)]
+#[case("resources/test_graphs/undirected/Graph_ganzganzgross.txt", 1000000)]
+fn create_from_file_creates_all_vertices(
+    #[case] input_path: &str,
+    #[case] expected_vertices: usize,
+) {
+    let graph = Graph::from_hoever_file(input_path, false).unwrap();
     let vertices = graph.get_all_vertices();
-    assert_eq!(vertices.len(), 15);
+    assert_eq!(vertices.len(), expected_vertices);
 }
 
 #[rstest]
