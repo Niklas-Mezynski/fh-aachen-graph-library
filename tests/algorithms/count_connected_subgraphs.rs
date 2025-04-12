98a2e9c2027203ee3d7fd5406e24a3245cd5bc5b
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2025-04-12 13:33:38 +0200

    Check num of connected subgraphs

--- tests/algorithms/count_connected_subgraphs.rs
+++ tests/algorithms/count_connected_subgraphs.rs
@@ -30,3 +30,41 @@ fn count_connected_subgraphs(
         input_path, traversal_type, expected_count, count
     );
 }
+
+#[rstest]
+#[case("resources/test_graphs/undirected_weighted/G_1_2.txt", 1)]
+#[case("resources/test_graphs/undirected_weighted/G_1_20.txt", 1)]
+#[case("resources/test_graphs/undirected_weighted/G_1_200.txt", 1)]
+#[case("resources/test_graphs/undirected_weighted/G_10_20.txt", 1)]
+#[case("resources/test_graphs/undirected_weighted/G_10_200.txt", 1)]
+#[case("resources/test_graphs/undirected_weighted/G_100_200.txt", 1)]
+fn count_connected_subgraphs_p02_undirected_weights(
+    #[case] input_path: &str,
+    #[case] expected_count: u32,
+    #[values(TraversalType::BFS, TraversalType::DFS)]
+    // Add other traversal types as needed
+    traversal_type: TraversalType,
+) {
+    use graph_library::graph::WeightedEdge;
+
+    let graph = Graph::from_hoever_file_with_weights(input_path, false, |remaining| {
+        WeightedEdge::new(
+            remaining[0]
+                .parse()
+                .expect("Graph file value must be a float"),
+        )
+    })
+    .unwrap_or_else(|e| panic!("Graph could not be constructed from file: {:?}", e));
+
+    // Count connected subgraphs
+    let count = graph
+        .count_connected_subgraphs(Some(traversal_type))
+        .unwrap_or_else(|e| panic!("Failed to count connected subgraphs: {:?}", e));
+
+    // Verify expected count
+    assert_eq!(
+        count, expected_count,
+        "For graph {} using {:?}, expected {} connected subgraphs, but got {}",
+        input_path, traversal_type, expected_count, count
+    );
+}
