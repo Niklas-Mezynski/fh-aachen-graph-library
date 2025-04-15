e9fe36e4f91e6bf12da0a7919a85a003119e0b1e
Niklas-Mezynski <70883487+Niklas-Mezynski@users.noreply.github.com>
2025-04-15 19:14:01 +0200

    Add pseudo code

--- src/algorithms/mst/kruskal.rs
+++ src/algorithms/mst/kruskal.rs
@@ -17,7 +17,10 @@ where
     pub fn mst_kruskal(&self) -> Result<Graph<VId, Vertex, Edge>, GraphError<VId>> {
         let mut mst_graph = Graph::<VId, Vertex, Edge>::new(self.is_directed());
 
-        todo!();
+        // Get all edges and sort them ascending
+        // Pop each edge in edges (lowest first):
+        //  if adding e to MST would not create a circle:
+        //      Add e to the MST
 
         Ok(mst_graph)
     }
